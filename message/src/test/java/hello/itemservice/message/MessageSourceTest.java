d079fedf99fd713a9e5a63b32bd3d01422f49801
syshinkr <syshinkr228@gmail.com>
2023-06-16 16:10:01 +0900

    웹 애플리케이션에 메시지 적용하기

--- message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,6 +1,5 @@
 package hello.itemservice.message;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +8,8 @@ import org.springframework.context.NoSuchMessageException;
 
 import java.util.Locale;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class MessageSourceTest {
