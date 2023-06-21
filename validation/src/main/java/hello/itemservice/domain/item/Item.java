c85a9dfd39b0a5d610466b11e7e5671fe0d363cd
syshinkr <syshinkr228@gmail.com>
2023-06-21 19:31:43 +0900

    Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+// ScriptAssert 는 기능이 약해 자바 코드 작성 권장
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요")
 public class Item {
 
     private Long id;
