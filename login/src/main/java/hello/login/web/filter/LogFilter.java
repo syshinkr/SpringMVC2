4c0e2b9ec7625e69d4f0cce29e90a8858bc103cf
syshinkr <syshinkr228@gmail.com>
2023-06-28 12:17:07 +0900

    서블릿 필터 - 요청 로그

---
+++ login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,44 @@
+package hello.login.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            // 다음 필터 또는 서블릿 호출
+            // 필터가 서블릿보다 앞단에 있기 때문에 꼭 호출해야 하는 로직
+            // logback mdc : 실무에서 같은 http 요청의 로그에 같은 식별자를 자동으로 남기기
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
