b623c71132c8a8b0977d8d0a8b7d622fe13fde84
syshinkr <syshinkr228@gmail.com>
2023-06-29 12:53:56 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
+//        1. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+//        2. WAS `/error-page/500` 다시 요청 -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(/errorPage/500) -> View
+
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
