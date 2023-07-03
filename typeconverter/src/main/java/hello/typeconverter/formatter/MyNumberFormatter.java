22101308e61ad6f6460ac1c6568dbbec92efeeac
syshinkr <syshinkr228@gmail.com>
2023-07-03 18:21:22 +0900

    포맷터 - Formatter

---
+++ typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+// Converter 는 범용(객체 객체)
+// Formatter 는 문자에 특화(객체 문자, 문자 객체) + 현지화(Locale) ; Converter 의 특별한 버전
+
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> {
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        // "1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        return NumberFormat.getInstance(locale).format(object);
+    }
+}
